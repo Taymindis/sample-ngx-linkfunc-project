9a9886ecf3efd788408d49c8294097997858380a
taymindis <taymindis@gmail.com>
2018-12-26 07:04:48 +0800

    update source refactoring

--- ngx-cfunc-ggl-direction/src/main/CfuncGglDirection.cpp
+++ ngx-cfunc-ggl-direction/src/main/CfuncGglDirection.cpp
@@ -11,7 +11,7 @@ extern "C" {
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#include <ngx_http_c_func_module.h>
+#include <ngx_link_func_module.h>
 
 
 extern Json::Value appConfiguration;
@@ -24,12 +24,12 @@ static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *use
 }
 
 
-void getHeartBeat(ngx_http_c_func_ctx_t* ctx) {
-    ngx_http_c_func_write_resp(
+void getHeartBeat(ngx_link_func_ctx_t* ctx) {
+    ngx_link_func_write_resp(
         ctx,
         200,
         NULL,
-        ngx_http_c_func_content_type_plaintext,
+        ngx_link_func_content_type_plaintext,
         "OK",
         sizeof("OK") - 1
     );
@@ -37,27 +37,27 @@ void getHeartBeat(ngx_http_c_func_ctx_t* ctx) {
 }
 
 
-void getDelay(ngx_http_c_func_ctx_t* ctx) {
-    ngx_http_c_func_write_resp(
+void getDelay(ngx_link_func_ctx_t* ctx) {
+    ngx_link_func_write_resp(
         ctx,
         200,
         NULL,
-        ngx_http_c_func_content_type_plaintext,
+        ngx_link_func_content_type_plaintext,
         "OK",
         sizeof("OK") - 1
     );
 }
 
-void getRoute(ngx_http_c_func_ctx_t* ctx) {
+void getRoute(ngx_link_func_ctx_t* ctx) {
     if (ctx->req_args) {
-        char *slat = (char*) ngx_http_c_func_get_query_param(ctx, "startLat");
-        char *slng = (char*) ngx_http_c_func_get_query_param(ctx, "startLng");
-        char *elat = (char*) ngx_http_c_func_get_query_param(ctx, "endLat");
-        char *elng = (char*) ngx_http_c_func_get_query_param(ctx, "endLng");
+        char *slat = (char*) ngx_link_func_get_query_param(ctx, "startLat");
+        char *slng = (char*) ngx_link_func_get_query_param(ctx, "startLng");
+        char *elat = (char*) ngx_link_func_get_query_param(ctx, "endLat");
+        char *elng = (char*) ngx_link_func_get_query_param(ctx, "endLng");
 
 
         if (slat && slng && elat && elng) {
-            ngx_http_c_func_log(info, ctx, "Args are %s, %s, %s, %s", slat, slng, elat, elng);
+            ngx_link_func_log(info, ctx, "Args are %s, %s, %s, %s", slat, slng, elat, elng);
 
             // double startLat = atof(slat);
             // double startlng = atof(slng);
@@ -81,7 +81,7 @@ void getRoute(ngx_http_c_func_ctx_t* ctx) {
 
                 char *fullUrl = (char*)url.c_str();
 
-                ngx_http_c_func_log(info, ctx, "fullUrl = %s", fullUrl);
+                ngx_link_func_log(info, ctx, "fullUrl = %s", fullUrl);
 
                 bcl::setOpts(req, CURLOPT_URL , fullUrl,
                              // CURLOPT_USERAGENT, "Mozilla/5.0",
@@ -91,11 +91,11 @@ void getRoute(ngx_http_c_func_ctx_t* ctx) {
                              CURLOPT_WRITEDATA, req->dataPtr
                             );
             }, [&](bcl::Response * resp) {
-                ngx_http_c_func_log(info, ctx, "Status Code= %ld", resp->code);
+                ngx_link_func_log(info, ctx, "Status Code= %ld", resp->code);
 
                 char* resultStr = (char*)resp->getBody<std::string>()->c_str();
 
-                // ngx_http_c_func_log(info, ctx,"Result = %s", resultStr);
+                // ngx_link_func_log(info, ctx,"Result = %s", resultStr);
 
                 Json::Value rs;
                 Json::Reader reader;
@@ -103,12 +103,12 @@ void getRoute(ngx_http_c_func_ctx_t* ctx) {
                 if ( !parsingSuccessful )
                 {
 
-                    ngx_http_c_func_log(err, ctx, "Failed to parse: %s", reader.getFormattedErrorMessages().c_str());
-                    ngx_http_c_func_write_resp(
+                    ngx_link_func_log(err, ctx, "Failed to parse: %s", reader.getFormattedErrorMessages().c_str());
+                    ngx_link_func_write_resp(
                         ctx,
                         200,
                         NULL,
-                        ngx_http_c_func_content_type_json,
+                        ngx_link_func_content_type_json,
                         "{'status':0}",
                         sizeof("{'status':0}") - 1
                     );
@@ -129,11 +129,11 @@ void getRoute(ngx_http_c_func_ctx_t* ctx) {
 
                         Json::FastWriter fastWriter;
                         std::string output = fastWriter.write(shrinkResult);
-                        ngx_http_c_func_write_resp(
+                        ngx_link_func_write_resp(
                             ctx,
                             200,
                             NULL,
-                            ngx_http_c_func_content_type_json,
+                            ngx_link_func_content_type_json,
                             output.c_str(),
                             output.length()
                         );
@@ -142,22 +142,22 @@ void getRoute(ngx_http_c_func_ctx_t* ctx) {
             });
 
         } else {
-            ngx_http_c_func_write_resp(
+            ngx_link_func_write_resp(
                 ctx,
                 200,
                 NULL,
-                ngx_http_c_func_content_type_json,
+                ngx_link_func_content_type_json,
                 "{'status':0}",
                 sizeof("{'status':0}") - 1
             );
         }
 
     } else {
-        ngx_http_c_func_write_resp(
+        ngx_link_func_write_resp(
             ctx,
             200,
             NULL,
-            ngx_http_c_func_content_type_json,
+            ngx_link_func_content_type_json,
             "{'status':0}",
             sizeof("{'status':0}") - 1
         );
@@ -165,7 +165,8 @@ void getRoute(ngx_http_c_func_ctx_t* ctx) {
 }
 
 
-void ngx_http_c_func_init(ngx_http_c_func_ctx_t *ctx) {
+void
+ngx_link_func_init_cycle(ngx_link_func_cycle_t* cyc) {
     bcl::init();
 
     Json::Reader reader;
@@ -173,14 +174,15 @@ void ngx_http_c_func_init(ngx_http_c_func_ctx_t *ctx) {
     do {
         std::ifstream file("/etc/nginx/app_config.json");
         if (!reader.parse(file, appConfiguration, false)) {
-            ngx_http_c_func_log(info, ctx, "Failed to parse configuration\n %s", reader.getFormattedErrorMessages().c_str());
+            ngx_link_func_cyc_log(info, cyc, "Failed to parse configuration\n %s", reader.getFormattedErrorMessages().c_str());
         }
     } while (0);
 }
 
 
-void ngx_http_c_func_exit(ngx_http_c_func_ctx_t *ctx) {
-    ngx_http_c_func_log_info(ctx, "Service is shutting down...........................................");
+void
+ngx_link_func_exit_cycle(ngx_link_func_cycle_t* cyc) {
+    ngx_link_func_cyc_log_info(cyc, "Service is shutting down...........................................");
 
     bcl::cleanUp();
 }
